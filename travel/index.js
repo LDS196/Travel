d1f4550a75054a8a4b0a565fe618b1c51a7f2475
LDS196 <lihachevd87@gmail.com>
2022-07-15 18:26:13 +0400

    feat: desctop slider distination

--- travel/index.js
+++ travel/index.js
@@ -14,19 +14,82 @@
    })
 }());
 
-console.log("1.48\n2.15\n3.22\nTotal:85");
+//slider desktop
+   let cerclePic =document.querySelectorAll('.destination-mover-cercle-pic') ;
+   function addActiveLeft () {
+      cerclePic[0].classList.add('destination-mover-cercle-active');
+      cerclePic[1].classList.remove('destination-mover-cercle-active');
+      cerclePic[2].classList.remove('destination-mover-cercle-active');
+   };
+   function addActiveRight () {
+      cerclePic[2].classList.add('destination-mover-cercle-active');
+      cerclePic[0].classList.remove('destination-mover-cercle-active');
+      cerclePic[1].classList.remove('destination-mover-cercle-active');
+   };
+   function removeActive () {
+      cerclePic[1].classList.add('destination-mover-cercle-active');
+      cerclePic[2].classList.remove('destination-mover-cercle-active');
+      cerclePic[0].classList.remove('destination-mover-cercle-active');
+   };
 
-//slider
-let offset = 0;
-const sliderLine = document.querySelectorAll('.destination-pic');
-console.log(sliderLine);
-document.querySelector('.distination-card').addEventListener('click', function () {
-   offset = offset + 860;
-   if (offset > 2720){
-      offset = 0;
-   }
-   sliderLine[0].style.left = offset + 'px';
-   sliderLine[1].style.left = offset + 'px';
-   sliderLine[2].style.left = -offset * 2 + 'px';
+   let slides = document.querySelectorAll('.destination-pic');
+   let slider = [];
+   for( let i=0; i < slides.length; i++){
+   slider[i] = slides[i].src
+   };
+   let step = 1;
+   function changeLeft(){
+      if (step === 1){
+         addActiveLeft ()
+         slides[0].src = slider[2];
+         slides[1].src = slider[0];
+         slides[2].src = slider[1];
+         }else if (step === 2) {
+            addActiveRight ()
+            slides[0].src = slider[1];
+            slides[1].src = slider[2];
+            slides[2].src = slider[0];
+         } else if (step === 3)  {
+            removeActive ()
+            slides[0].src = slider[0];
+            slides[1].src = slider[1];
+            slides[2].src = slider[2];
+         }  step++
+      if(step <= slides.length){
+         
+      } else step = +1;
+   
+};
 
+function changeRight(){
+   if (step === 1){
+      addActiveRight()
+      slides[0].src = slider[1];
+      slides[1].src = slider[2];
+      slides[2].src = slider[0];
+      }else if (step === 2) {
+         addActiveLeft ()
+         slides[0].src = slider[2];
+         slides[1].src = slider[0];
+         slides[2].src = slider[1];
+      } else if (step === 3)  {
+         removeActive ()
+         slides[0].src = slider[0];
+         slides[1].src = slider[1];
+         slides[2].src = slider[2];
+      } step++;
+   if(step <= slides.length){
+
+   } else step = +1;
+
+};
+
+document.querySelector('.distination-card-slider-left').addEventListener('click', function () {
+changeLeft();
 });
+
+document.querySelector('.distination-card-slider-right').addEventListener('click', function () {
+   changeRight();
+   });
+
+   //End slider desctop
